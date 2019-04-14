53681ddfbe29a8b7b308d1c0830a06b6dd15166c
Rafael Madureira dos Santos <rafael.madureira.santos@gmail.com>
2019-04-14 17:41:37 -0300

    Add Java's Decorator pattern example

---
+++ structural/Java/Decorator.java
@@ -0,0 +1,100 @@
+interface Widget {
+
+	public void draw();
+
+	public String getDescription();
+
+}
+
+class Window implements Widget {
+
+	@Override
+	public void draw() {
+		System.out.println("Drawing the Window");
+	}
+
+	@Override
+	public String getDescription() {
+		return "Window";
+	}
+
+}
+
+class WindowDecorator extends Window {
+
+	private Widget windowToBeDecorated;
+
+	public WindowDecorator(Widget windowToBeDecorated) {
+		this.windowToBeDecorated = windowToBeDecorated;
+	}
+
+	@Override
+	public void draw() {
+		this.windowToBeDecorated.draw(); // Delegation
+	}
+
+	public String getDescription() {
+		return this.windowToBeDecorated.getDescription(); // Delegation
+	}
+
+}
+
+class VerticalScrollBarDecorator extends WindowDecorator {
+
+	public VerticalScrollBarDecorator(Widget windowToBeDecorated) {
+		super(windowToBeDecorated);
+	}
+
+	@Override
+	public void draw() {
+		super.draw();
+		this.drawVerticalScrollBar();
+	}
+
+	@Override
+	public String getDescription() {
+		return super.getDescription() + ", including vertical scrollbars";
+	}
+
+	private void drawVerticalScrollBar() {
+		System.out.println("Drawing Vertical ScrollBar");
+	}
+
+}
+
+class HorizontalScrollBarDecorator extends WindowDecorator {
+
+	public HorizontalScrollBarDecorator(Widget windowToBeDecorated) {
+		super(windowToBeDecorated);
+	}
+
+	@Override
+	public void draw() {
+		super.draw();
+		this.drawHorizontalScrollBar();
+	}
+
+	@Override
+	public String getDescription() {
+		return super.getDescription() + ", including horizontal scrollbars";
+	}
+
+	private void drawHorizontalScrollBar() {
+		System.out.println("Drawing Horizontal ScrollBar");
+	}
+
+}
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println(":: Decorator ::\n");
+
+		System.out.println(":: Decorator ::\n");
+
+		Window decoratedWindow = new HorizontalScrollBarDecorator(new VerticalScrollBarDecorator(new Window()));
+
+		System.out.println(decoratedWindow.getDescription());
+	}
+
+}
